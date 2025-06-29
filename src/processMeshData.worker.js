2fa67fb1af912471a72ea2e3346384c787465071
-PT_AxH_RV- <140041025+1-PT-AxH-RV-0@users.noreply.github.com>
2025-06-29 14:21:10 +0800

    使用 WebWorker 实现了异步处理网格数据，并添加了处理进度条。
    还更新了 README.md.

---
+++ src/processMeshData.worker.js
@@ -0,0 +1,21 @@
+import { processMeshData } from './offProcessor.js';
+import { process4DMeshData } from './offProcessor4D.js';
+
+self.addEventListener('message', event => {
+  const { meshData, is4D } = event.data;
+  
+  let progress = 0;
+    
+  try {
+    const func = is4D ? process4DMeshData : processMeshData;
+    
+    const result = func(meshData, (current, total) => {
+      progress = current / total * 100;
+      self.postMessage({ type: 'progress', data: progress });
+    });
+    
+    self.postMessage({ type: 'complete', data: result });
+  } catch (error) {
+    self.postMessage({ type: 'error', data: error });
+  }
+});
