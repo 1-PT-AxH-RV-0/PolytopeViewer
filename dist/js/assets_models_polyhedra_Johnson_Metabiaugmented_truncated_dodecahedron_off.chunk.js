d0cb362402f3cfad1de8517eea1e5619331d4630
-PT_AxH_RV- <140041025+1-PT-AxH-RV-0@users.noreply.github.com>
2025-07-08 20:03:43 +0800

    添加约翰逊多面体。
    修复切换标签页时控制页的间隔失效的问题。

---
+++ dist/js/assets_models_polyhedra_Johnson_Metabiaugmented_truncated_dodecahedron_off.chunk.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["assets_models_polyhedra_Johnson_Metabiaugmented_truncated_dodecahedron_off"],{
+
+/***/ "./assets/models/polyhedra/Johnson/Metabiaugmented_truncated_dodecahedron.off":
+/*!************************************************************************************!*\
+  !*** ./assets/models/polyhedra/Johnson/Metabiaugmented_truncated_dodecahedron.off ***!
+  \************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"assets/Metabiaugmented_truncated_dodecahedron.off\";\n\n//# sourceURL=webpack:///./assets/models/polyhedra/Johnson/Metabiaugmented_truncated_dodecahedron.off?");
+
+/***/ })
+
+}]);
